a35e03b9611da2307b9e710a2e24a6e8a20bb575
Housa <alexandra@housa.dk>
2018-06-03 17:29:09 +0200

    now with working example of skSNARK

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#define DEBUG true
+
 #include <cassert>
 #include <cstdio>
 
@@ -10,39 +12,15 @@
 #include <libsnark/relations/constraint_satisfaction_problems/r1cs/examples/r1cs_examples.hpp>
 #include <libsnark/zk_proof_systems/ppzksnark/r1cs_ppzksnark/examples/run_r1cs_ppzksnark.hpp>
 #include "suppe/snark.h"
-#include "suppe/sha_test.h"
+#include "suppe/webstrate_cs.h"
 #include "suppe/picosha2.h"
+
+#define BIT(N, x) (((x) >> (N)) & 1)
 
 using namespace libsnark;
 
-//original test code taken from libsnark to be altered to fit use case
-//TODO step 1: use own R1CS instead of example
-template<typename ppT>
-void test_r1cs_ppzksnark(size_t num_constraints,
-                         size_t input_size)
-{
-    libff::print_header("(enter) Run R1CS ppzkSNARK");
-
-    const bool test_serialization = true;
-
-    //R1CS part
-    r1cs_example<libff::Fr<ppT> > example = generate_r1cs_example_with_binary_input<libff::Fr<ppT> >(num_constraints, input_size);
-
-    //zksnark part
-    bp::Fisk<ppT> fisk;
-    //generate
-    r1cs_ppzksnark_keypair<ppT> keypair = fisk.generate(example.constraint_system, test_serialization);
-    //prove
-    libsnark::r1cs_ppzksnark_proof<ppT> proof = fisk.prove(keypair.pk, example.primary_input, example.auxiliary_input, test_serialization);
-    //verify
-    bool ans = fisk.verify(keypair.vk, example.primary_input, proof);
-    assert(ans);
-
-    libff::print_header("(leave) Run R1CS ppzkSNARK");
-}
-
 template<typename FieldT>
-std::vector<FieldT> generate_input(const long int *vals, size_t cnt, size_t max)
+std::vector<FieldT> generate_input(const long *vals, size_t cnt, size_t max)
 {
     r1cs_variable_assignment<FieldT> full_variable_assignment;
 
@@ -50,52 +28,35 @@ std::vector<FieldT> generate_input(const long int *vals, size_t cnt, size_t max)
         full_variable_assignment.push_back(FieldT(vals[j]));
     }
     for (int k = 0; k < max - cnt; ++k) {
-        full_variable_assignment.push_back(FieldT::zero());
+        if (k == 0) {
+            full_variable_assignment.push_back(FieldT(0x80000000));
+        } else {
+            full_variable_assignment.push_back(FieldT(0));
+        }
     }
 
     return std::vector<FieldT>(full_variable_assignment.begin(), full_variable_assignment.begin() + max);
 }
 
-long int eight_ints_to_long_int(long int i0, long int i1, long int i2, long int i3, long int i4, long int i5, long int i6, long int i7) {
-
-    return (i0 << 56) | (i1 << 48) | (i2 << 40) | (i3 << 32) | (i4 << 24) | (i5 << 16) | (i6 << 8) | i7;
-}
-
-template<typename ppT>
-void test_r1cs_websnark(size_t input_size)
+template<typename FieldT>
+std::vector<FieldT> generate_bit_vec_input(const long int *vals, size_t cnt, size_t max)
 {
+    r1cs_variable_assignment<FieldT> full_variable_assignment;
 
-    typedef default_r1cs_ppzksnark_pp detype;
-    typedef libff::Fr<detype> FieldT;
+    int input_size = 32*cnt;
 
-    //R1CS part
-    libsnark::r1cs_constraint_system<FieldT> cs = bp::gen_webstrate_constraint_system<FieldT>(input_size);
+    for (int j = 0; j < cnt; ++j) {
+        for (int k = 31; k >= 0 ; --k) {
+            full_variable_assignment.push_back(FieldT(BIT(k, vals[j])));
+        }
+    }
+    for (int k = 0; k < max - input_size; ++k) {
+        full_variable_assignment.push_back(FieldT(0));
+    }
 
-    cs.primary_input_size = 4;
-    cs.auxiliary_input_size -= cs.primary_input_size;
-
-    //zksnark part
-    bp::Fisk<ppT> fisk;
-    //generate
-    r1cs_ppzksnark_keypair<ppT> keypair = fisk.generate(cs, true);
-
-    //create test input
-    const long int input[] = {'1', '2'};
-    libsnark::r1cs_auxiliary_input<FieldT> auxiliary_input = generate_input<FieldT>(input, 2, cs.auxiliary_input_size);
-
-    std::string src_str = "12";
-
-    std::vector<long int> hash(picosha2::k_digest_size);
-    picosha2::hash256(src_str.begin(), src_str.end(), hash.begin(), hash.end());
-
-    std::string hex_str = picosha2::bytes_to_hex_string(hash.begin(), hash.end());
-
-    libsnark::r1cs_primary_input<FieldT> primary_input = generate_input<FieldT>(&hash[0], 4, 4);
-    std::cout << hex_str;
-
-    //prove
-    libsnark::r1cs_ppzksnark_proof<ppT> proof = fisk.prove(keypair.pk, primary_input, auxiliary_input, true);
+    return std::vector<FieldT>(full_variable_assignment.begin(), full_variable_assignment.begin() + max);
 }
+
 
 template<typename ppT>
 void test_r1cs_minimal(size_t input_size)
@@ -105,26 +66,32 @@ void test_r1cs_minimal(size_t input_size)
     typedef libff::Fr<detype> FieldT;
 
     //R1CS part
-    libsnark::r1cs_constraint_system<FieldT> cs = bp::gen_webstrate_constraint_system_minimal<FieldT>(input_size);
+    bp::WebstrateSnark<FieldT> webstrateSnark(input_size);
+    webstrateSnark.generate_r1cs_constraints();
 
-    cs.primary_input_size = 1;
-    cs.auxiliary_input_size -= cs.primary_input_size;
+    webstrateSnark.set_num_of_inputs(1);
+
+    libsnark::r1cs_constraint_system<FieldT> cs = webstrateSnark.get_constraint_system();
+
 
     //zksnark part
     bp::Fisk<ppT> fisk;
+
     //generate
     r1cs_ppzksnark_keypair<ppT> keypair = fisk.generate(cs, true);
 
     //create test input
+    const long int primary_input_vals[] = {0xd23caea};
 
-    const long int input[] = {42};
-    libsnark::r1cs_primary_input<FieldT> primary_input = generate_input<FieldT>(input, 1, 1);
+    libsnark::r1cs_primary_input<FieldT> primary_input = generate_input<FieldT>(primary_input_vals, 1, 1);
 
-    const long int aux_input[] = {2, 21};
-    libsnark::r1cs_auxiliary_input<FieldT> auxiliary_input = generate_input<FieldT>(aux_input, 2, cs.auxiliary_input_size);
+    const long int aux_input[] = {'2', '1', '2', '1'};
+    libsnark::r1cs_auxiliary_input<FieldT> auxiliary_input = generate_bit_vec_input<FieldT>(aux_input, 4, cs.auxiliary_input_size);
+
+    libsnark::r1cs_auxiliary_input<FieldT> aux_input_v2 = webstrateSnark.generate_r1cs_witness(primary_input, auxiliary_input);
 
     //prove
-    libsnark::r1cs_ppzksnark_proof<ppT> proof = fisk.prove(keypair.pk, primary_input, auxiliary_input, true);
+    libsnark::r1cs_ppzksnark_proof<ppT> proof = fisk.prove(keypair.pk, primary_input, aux_input_v2, true);
 
     //verify
     bool ans = fisk.verify(keypair.vk, primary_input, proof);
@@ -137,21 +104,6 @@ int main(int argc, const char * argv[])
     default_r1cs_ppzksnark_pp::init_public_params();
     libff::start_profiling();
 
-    //test_r1cs_ppzksnark<default_r1cs_ppzksnark_pp>(1000, 100);
 
-    //test_r1cs_websnark<default_r1cs_ppzksnark_pp>(1000);
-
-    test_r1cs_minimal<default_r1cs_ppzksnark_pp>(1000);
+    test_r1cs_minimal<default_r1cs_ppzksnark_pp>(512);
 }
-
-/*
- *
-    default_r1cs_ppzksnark_pp::init_public_params();
-    libff::start_profiling();
-
-    typedef default_r1cs_ppzksnark_pp detype;
-
-    r1cs_example<libff::Fr<detype>> example = gen_sha256_example<libff::Fr<detype>>(1000);
-
-    const bool bit = run_r1cs_ppzksnark<detype>(example, true);
- */
---
+++ src/suppe/webstrate_cs.h
@@ -0,0 +1,104 @@
+//
+// Created by housa on 5/14/18.
+//
+
+#ifndef PROJECT_SHA_TEST_H
+#define PROJECT_SHA_TEST_H
+
+#include <cstdio>
+
+#include <libsnark/relations/constraint_satisfaction_problems/r1cs/examples/r1cs_examples.hpp>
+#include <libsnark/gadgetlib1/protoboard.hpp>
+#include <libsnark/gadgetlib1/gadgets/hashes/hash_io.hpp>
+#include <libsnark/gadgetlib1/gadgets/hashes/sha256/sha256_gadget.hpp>
+
+namespace bp {
+
+    template<typename FieldT>
+    class WebstrateSnark {
+    private:
+        std::shared_ptr<libsnark::protoboard<FieldT>> pb;
+        std::shared_ptr<libsnark::pb_variable<FieldT>> primary_in;
+        std::shared_ptr<libsnark::pb_variable_array<FieldT>> witness;
+        std::shared_ptr<libsnark::digest_variable<FieldT>> out;
+        std::shared_ptr<libsnark::pb_variable<FieldT>> lc_aux1;
+        std::shared_ptr<libsnark::packing_gadget<FieldT>> pg1;
+
+        std::shared_ptr<libsnark::sha256_compression_function_gadget<FieldT>> sha256;
+
+    public:
+        WebstrateSnark(const size_t tree_size) {
+            // SHA blocks
+            assert(tree_size % 512 == 0);
+
+            pb = std::shared_ptr<libsnark::protoboard<FieldT>>(new libsnark::protoboard<FieldT>);
+
+            primary_in = std::shared_ptr<libsnark::pb_variable<FieldT>>(new libsnark::pb_variable<FieldT>);
+            witness = std::shared_ptr<libsnark::pb_variable_array<FieldT>>(new libsnark::pb_variable_array<FieldT>);
+
+            lc_aux1 = std::shared_ptr<libsnark::pb_variable<FieldT>>(new libsnark::pb_variable<FieldT>);
+
+
+            primary_in->allocate(*pb, "in");
+            witness->allocate(*pb, tree_size, "witness");
+
+            lc_aux1->allocate(*pb, "lc_aux1");
+
+            libsnark::pb_linear_combination<FieldT> packed_result1(*lc_aux1);
+
+            out = std::shared_ptr<libsnark::digest_variable<FieldT>>(
+                    new libsnark::digest_variable<FieldT>(*pb, 256, "output"));
+
+
+            sha256 = std::shared_ptr<libsnark::sha256_compression_function_gadget<FieldT>>(
+                    new libsnark::sha256_compression_function_gadget<FieldT>(*pb,
+                                                                             libsnark::SHA256_default_IV(*pb),
+                                                                             *witness,
+                                                                             *out,
+                                                                             "sha"));
+
+            libsnark::pb_variable_array<FieldT> lc_arr(out->bits);
+            int i = 0;
+            libsnark::pb_variable_array<FieldT> lc_arr1(lc_arr.rbegin() + i * 32, lc_arr.rbegin() + (i + 1) * 32);
+
+            pg1 = std::shared_ptr<libsnark::packing_gadget<FieldT>>(new libsnark::packing_gadget<FieldT>(*pb, lc_arr1, packed_result1, "packing1"));
+        }
+
+        void set_num_of_inputs(const size_t num_inputs) {
+            pb->set_input_sizes(num_inputs);
+        }
+
+        void generate_r1cs_constraints() {
+            sha256->generate_r1cs_constraints();
+
+            pg1->generate_r1cs_constraints(1);
+
+            pb->add_r1cs_constraint(libsnark::r1cs_constraint<FieldT>(1, *lc_aux1, *primary_in),
+                                    "eq constraint");
+        }
+
+        libsnark::r1cs_auxiliary_input<FieldT> generate_r1cs_witness(libsnark::r1cs_primary_input<FieldT> input,
+                                                                     libsnark::r1cs_auxiliary_input<FieldT> auxiliary_input) {
+            pb->val(*primary_in) = input[0];
+
+            for (int i = 0; i < witness->size(); ++i) {
+                pb->val((*witness)[i]) = auxiliary_input[i];
+            }
+
+            sha256->generate_r1cs_witness();
+
+            pg1->generate_r1cs_witness_from_bits();
+
+            printf("FiskeMaster: %lx \n", pb->val(*lc_aux1).as_ulong());
+
+            return pb->auxiliary_input();
+        }
+
+        libsnark::r1cs_constraint_system<FieldT> get_constraint_system() {
+            return pb->get_constraint_system();
+        }
+    };
+
+}
+
+#endif //PROJECT_SHA_TEST_H
