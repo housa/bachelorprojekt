a3d831fed810f151edba0705b6a44f3045adf27c
Housa <alexandra@housa.dk>
2018-06-03 12:32:46 +0200

    a constraintsystem with input that passes

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,8 @@
 #include <libsnark/relations/constraint_satisfaction_problems/r1cs/examples/r1cs_examples.hpp>
 #include <libsnark/zk_proof_systems/ppzksnark/r1cs_ppzksnark/examples/run_r1cs_ppzksnark.hpp>
 #include "suppe/snark.h"
+#include "suppe/sha_test.h"
+#include "suppe/picosha2.h"
 
 using namespace libsnark;
 
@@ -39,12 +41,107 @@ void test_r1cs_ppzksnark(size_t num_constraints,
     libff::print_header("(leave) Run R1CS ppzkSNARK");
 }
 
+template<typename FieldT>
+std::vector<FieldT> generate_input(const long int *vals, size_t cnt, size_t max)
+{
+    r1cs_variable_assignment<FieldT> full_variable_assignment;
+
+    for (int j = 0; j < cnt; ++j) {
+        full_variable_assignment.push_back(FieldT(vals[j]));
+    }
+    for (int k = 0; k < max - cnt; ++k) {
+        full_variable_assignment.push_back(FieldT::zero());
+    }
+
+    return std::vector<FieldT>(full_variable_assignment.begin(), full_variable_assignment.begin() + max);
+}
+
+long int eight_ints_to_long_int(long int i0, long int i1, long int i2, long int i3, long int i4, long int i5, long int i6, long int i7) {
+
+    return (i0 << 56) | (i1 << 48) | (i2 << 40) | (i3 << 32) | (i4 << 24) | (i5 << 16) | (i6 << 8) | i7;
+}
+
+template<typename ppT>
+void test_r1cs_websnark(size_t input_size)
+{
+
+    typedef default_r1cs_ppzksnark_pp detype;
+    typedef libff::Fr<detype> FieldT;
+
+    //R1CS part
+    libsnark::r1cs_constraint_system<FieldT> cs = bp::gen_webstrate_constraint_system<FieldT>(input_size);
+
+    cs.primary_input_size = 4;
+    cs.auxiliary_input_size -= cs.primary_input_size;
+
+    //zksnark part
+    bp::Fisk<ppT> fisk;
+    //generate
+    r1cs_ppzksnark_keypair<ppT> keypair = fisk.generate(cs, true);
+
+    //create test input
+    const long int input[] = {'1', '2'};
+    libsnark::r1cs_auxiliary_input<FieldT> auxiliary_input = generate_input<FieldT>(input, 2, cs.auxiliary_input_size);
+
+    std::string src_str = "12";
+
+    std::vector<long int> hash(picosha2::k_digest_size);
+    picosha2::hash256(src_str.begin(), src_str.end(), hash.begin(), hash.end());
+
+    std::string hex_str = picosha2::bytes_to_hex_string(hash.begin(), hash.end());
+
+    libsnark::r1cs_primary_input<FieldT> primary_input = generate_input<FieldT>(&hash[0], 4, 4);
+    std::cout << hex_str;
+
+    //prove
+    libsnark::r1cs_ppzksnark_proof<ppT> proof = fisk.prove(keypair.pk, primary_input, auxiliary_input, true);
+}
+
+template<typename ppT>
+void test_r1cs_minimal(size_t input_size)
+{
+
+    typedef default_r1cs_ppzksnark_pp detype;
+    typedef libff::Fr<detype> FieldT;
+
+    //R1CS part
+    libsnark::r1cs_constraint_system<FieldT> cs = bp::gen_webstrate_constraint_system_minimal<FieldT>(input_size);
+
+    cs.primary_input_size = 1;
+    cs.auxiliary_input_size -= cs.primary_input_size;
+
+    //zksnark part
+    bp::Fisk<ppT> fisk;
+    //generate
+    r1cs_ppzksnark_keypair<ppT> keypair = fisk.generate(cs, true);
+
+    //create test input
+
+    const long int input[] = {42};
+    libsnark::r1cs_primary_input<FieldT> primary_input = generate_input<FieldT>(input, 1, 1);
+
+    const long int aux_input[] = {2, 21};
+    libsnark::r1cs_auxiliary_input<FieldT> auxiliary_input = generate_input<FieldT>(aux_input, 2, cs.auxiliary_input_size);
+
+    //prove
+    libsnark::r1cs_ppzksnark_proof<ppT> proof = fisk.prove(keypair.pk, primary_input, auxiliary_input, true);
+
+    //verify
+    bool ans = fisk.verify(keypair.vk, primary_input, proof);
+    assert(ans);
+
+}
+
 int main(int argc, const char * argv[])
 {
     default_r1cs_ppzksnark_pp::init_public_params();
     libff::start_profiling();
 
-    test_r1cs_ppzksnark<default_r1cs_ppzksnark_pp>(1000, 100);
+    //test_r1cs_ppzksnark<default_r1cs_ppzksnark_pp>(1000, 100);
+
+    //test_r1cs_websnark<default_r1cs_ppzksnark_pp>(1000);
+
+    test_r1cs_minimal<default_r1cs_ppzksnark_pp>(1000);
 }
 
 /*
--- src/suppe/sha_test.h
+++ src/suppe/sha_test.h
@@ -15,30 +15,133 @@
 namespace bp {
 
     template<typename FieldT>
-    libsnark::r1cs_example<FieldT> gen_sha256_example(const size_t num_inputs) {
-        const size_t new_num_inputs = num_inputs - 1;
+    libsnark::r1cs_constraint_system<FieldT> gen_webstrate_constraint_system(const size_t tree_size) {
 
-/* construct dummy example: inner products of two vectors */
+        /* construct variables: */
         libsnark::protoboard<FieldT> pb;
-        libsnark::pb_variable_array<FieldT> in;
+        libsnark::pb_variable_array<FieldT> aux_in;
+        libsnark::pb_variable<FieldT> primary_in1;
+        libsnark::pb_variable<FieldT> primary_in2;
+        libsnark::pb_variable<FieldT> primary_in3;
+        libsnark::pb_variable<FieldT> primary_in4;
+        libsnark::pb_variable<FieldT> less_eq1;
+        libsnark::pb_variable<FieldT> less_eq2;
+        libsnark::pb_variable<FieldT> less_eq3;
+        libsnark::pb_variable<FieldT> less_eq4;
+        libsnark::pb_variable<FieldT> less1;
+        libsnark::pb_variable<FieldT> less2;
+        libsnark::pb_variable<FieldT> less3;
+        libsnark::pb_variable<FieldT> less4;
+        libsnark::pb_variable<FieldT> lc_aux1;
+        libsnark::pb_variable<FieldT> lc_aux2;
+        libsnark::pb_variable<FieldT> lc_aux3;
+        libsnark::pb_variable<FieldT> lc_aux4;
+        libsnark::pb_linear_combination<FieldT> packed_result1(lc_aux1);
+        libsnark::pb_linear_combination<FieldT> packed_result2(lc_aux2);
+        libsnark::pb_linear_combination<FieldT> packed_result3(lc_aux3);
+        libsnark::pb_linear_combination<FieldT> packed_result4(lc_aux4);
+
+        primary_in1.allocate(pb, "h(T)1");
+        primary_in2.allocate(pb, "h(T)2");
+        primary_in3.allocate(pb, "h(T)3");
+        primary_in4.allocate(pb, "h(T)4");
+        aux_in.allocate(pb, tree_size, "T");
+        lc_aux1.allocate(pb, "lc_aux");
+        lc_aux2.allocate(pb, "lc_aux");
+        lc_aux3.allocate(pb, "lc_aux");
+        lc_aux4.allocate(pb, "lc_aux");
+        less_eq1.allocate(pb, "leq1");
+        less_eq2.allocate(pb, "leq2");
+        less_eq3.allocate(pb, "leq3");
+        less_eq4.allocate(pb, "leq4");
+        less1.allocate(pb, "less1");
+        less2.allocate(pb, "less2");
+        less3.allocate(pb, "less3");
+        less4.allocate(pb, "less4");
         libsnark::digest_variable<FieldT> out(pb, 256, "output");
 
-        in.allocate(pb, new_num_inputs, "in");
-
-        libsnark::sha256_compression_function_gadget<FieldT> sha256(pb, libsnark::SHA256_default_IV<FieldT>(pb), in,
+        //hash part
+        libsnark::sha256_compression_function_gadget<FieldT> sha256(pb,
+                                                                    libsnark::SHA256_default_IV<FieldT>(pb),
+                                                                    aux_in,
                                                                     out,
                                                                     "sha");
         sha256.generate_r1cs_constraints();
+        sha256.generate_r1cs_witness();
 
-/* fill in random example */
-        for (size_t i = 0; i < new_num_inputs; ++i) {
-            pb.val(in[i]) = FieldT::random_element();
-        }
+        //comparison part
 
-//sha256.generate_r1cs_witness();
-//out.generate_r1cs_witness();
-        return libsnark::r1cs_example<FieldT>(pb.get_constraint_system(), pb.primary_input(), pb.auxiliary_input());
+        //create linear combination from digest
+        int i = 0;
+        libsnark::pb_linear_combination_array<FieldT> lc_arr(out.bits);
+        libsnark::pb_linear_combination_array<FieldT> lc_arr1(lc_arr.begin() + (i*64), lc_arr.begin() + (i+1) * 64);
+        i++;
+        libsnark::pb_linear_combination_array<FieldT> lc_arr2(lc_arr.begin() + (i*64), lc_arr.begin() + (i+1) * 64);
+        i++;
+        libsnark::pb_linear_combination_array<FieldT> lc_arr3(lc_arr.begin() + (i*64), lc_arr.begin() + (i+1) * 64);
+        i++;
+        libsnark::pb_linear_combination_array<FieldT> lc_arr4(lc_arr.begin() + (i*64), lc_arr.begin() + (i+1) * 64);
+
+        //pack linear combination of digest
+        libsnark::packing_gadget<FieldT> pg1(pb, lc_arr1, packed_result1, "packing");
+        libsnark::packing_gadget<FieldT> pg2(pb, lc_arr2, packed_result2, "packing");
+        libsnark::packing_gadget<FieldT> pg3(pb, lc_arr3, packed_result3, "packing");
+        libsnark::packing_gadget<FieldT> pg4(pb, lc_arr4, packed_result4, "packing");
+
+        pg1.generate_r1cs_constraints(1);
+        pg1.generate_r1cs_witness_from_packed();
+        pg2.generate_r1cs_constraints(1);
+        pg2.generate_r1cs_witness_from_packed();
+        pg3.generate_r1cs_constraints(1);
+        pg3.generate_r1cs_witness_from_packed();
+        pg4.generate_r1cs_constraints(1);
+        pg4.generate_r1cs_witness_from_packed();
+
+        //compare
+        libsnark::comparison_gadget<FieldT> cmp1(pb, 64, primary_in1, packed_result1, less1, less_eq1, "compare");
+        libsnark::comparison_gadget<FieldT> cmp2(pb, 64, primary_in2, packed_result2, less2, less_eq2, "compare");
+        libsnark::comparison_gadget<FieldT> cmp3(pb, 64, primary_in3, packed_result3, less3, less_eq3, "compare");
+        libsnark::comparison_gadget<FieldT> cmp4(pb, 64, primary_in4, packed_result4, less4, less_eq4, "compare");
+
+        cmp1.generate_r1cs_constraints();
+        cmp1.generate_r1cs_witness();
+        cmp2.generate_r1cs_constraints();
+        cmp2.generate_r1cs_witness();
+        cmp3.generate_r1cs_constraints();
+        cmp3.generate_r1cs_witness();
+        cmp4.generate_r1cs_constraints();
+        cmp4.generate_r1cs_witness();
+
+        //check comparison result
+        pb.add_r1cs_constraint(libsnark::r1cs_constraint<FieldT>(less_eq1, 1-less1, FieldT::one()));
+        pb.add_r1cs_constraint(libsnark::r1cs_constraint<FieldT>(less_eq2, 1-less2, FieldT::one()));
+        pb.add_r1cs_constraint(libsnark::r1cs_constraint<FieldT>(less_eq3, 1-less3, FieldT::one()));
+        pb.add_r1cs_constraint(libsnark::r1cs_constraint<FieldT>(less_eq4, 1-less4, FieldT::one()));
+
+        //return constraint system
+        return pb.get_constraint_system();
     }
+
+    template<typename FieldT>
+    libsnark::r1cs_constraint_system<FieldT> gen_webstrate_constraint_system_minimal(const size_t tree_size) {
+
+        /* construct variables: */
+        libsnark::protoboard<FieldT> pb;
+        libsnark::pb_variable<FieldT> primary_in;
+        libsnark::pb_variable<FieldT> aux_in;
+        libsnark::pb_variable<FieldT> aux_in2;
+
+        primary_in.allocate(pb, "in");
+        aux_in.allocate(pb, "aux in");
+        aux_in2.allocate(pb, "aux in2");
+
+
+        pb.add_r1cs_constraint(libsnark::r1cs_constraint<FieldT>(aux_in2, aux_in, primary_in));
+
+        //return constraint system
+        return pb.get_constraint_system();
+    }
+
 }
 
 #endif //PROJECT_SHA_TEST_H
